d508eb97ab0cef9fd88bcedcc43d0ce3f50db8c8
richorr <rich_orr@yahoo.com>
2016-12-30 21:32:57 -0700

    Added modem connect and bbs list commands
    Enhanced the commands supported to include the modem commands connect
    and list.

---
+++ bbsdefinition.py
@@ -0,0 +1,7 @@
+import json
+
+class BBSDefinition:
+	'Definition of BBS List elements used as elements within the game' 
+
+	def name(self):
+		return 
---
+++ command.py
@@ -0,0 +1,31 @@
+import json
+
+# Validates to see if the user possesses the appropriate POWER 
+# and PRESTIGE to perform the command
+def authCommand():
+	#TODO(richorr) Add the validation code
+	return true 
+
+def modemconnect(phone):
+	with open('data/bbs.json') as f:
+		bbsdata = json.load(f)
+	bbsToConnect = [bbs for bbs in bbsdata['bbslist'] if bbs['phone'] == phone]
+	print(bbsToConnect)
+	#TODO(richorr) Update user state
+
+def modemdisconnect(phone):
+	#TODO(richorr) Add disconnect code - reset the users connect state
+	return "not implemented"
+
+def modemlist(prestige):
+	with open('data/bbs.json') as f:
+		bbsdata = json.load(f)
+	count = 0
+	bbslist = []
+	for bbs in bbsdata['bbslist']:
+		if bbs['minPrestige'] <= prestige:
+			bbslist.append("[{}] {} - {}\n".format(count, bbs['name'], bbs['phone']))
+			print("[{}] {} - {}".format(count, bbs['name'], bbs['phone']))
+			count+=1
+	print(bbslist)
+
